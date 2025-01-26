cc3cfd0c8c7b832ea7361d321705cd39ed637c50
x64nik <rushidarunte123@gmail.com>
2025-01-26 19:19:45 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-zinc-950 text-zinc-100 antialiased`}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   )
