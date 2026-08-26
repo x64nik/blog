f9e5820e9ad963697fdf801a9063865a0ba9a839
x64nik <rushidarunte123@gmail.com>
2026-08-26 21:27:45 +0530

    vercel build fix

--- build.sh
+++ build.sh
@@ -16,6 +16,8 @@ main() {
 
   export TZ=Europe/Oslo
 
+  mkdir -p "${HOME}/.local"
+
   # Install Dart Sass
   echo "Installing Dart Sass ${DART_SASS_VERSION}..."
   curl -sLJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
